e58295b0d7b13e32c5d392b160f834e92291254c
jmk9633 <jmk9633@naver.com>
2022-05-23 23:21:37 +0900

    220523

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<header>
+        	<div className='header-container'> 
+            	<h1><a href='#'>K-FILX</a></h1>
+             	<nav className='header-nav'>
+					<ul>
+                		<li><a href='#'>HOME</a></li>
+                		<li><a href='#'>CATEGORY</a></li>
+                		<li><a href='#'>MY LIST </a></li>
+                		<li><a href='#'><img src={'ic_notification.png'} alt="alarm"></img></a></li>
+                		<li><a href='#'><img src={'ic_search.png'} alt="alarm"></img></a></li>
+					</ul>
+              </nav>
+          </div>
+    	</header>
+			<div className='img-title-container'>
+				<div className='main-title-container'>
+					<h1>Stove League</h1>
+					<p>A hot winter story in which the newly appointed leader of the last team, who has even the tears of fans, prepares for an extraordinary season</p>
+				</div>
+				<button>PLAY</button>
+				<button>DETAIL</button>
+			</div>
+	 <div className='background'>
+			<img src={'mainimage.jpg'}></img>
+	</div>
+
     </div>
   );
 }
